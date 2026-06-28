5df33c6056adf86b76ef55c560ee47f33479d61c
青山公仔 <qingshangongzai@163.com>
2026-06-28 18:03:44 +0800

    [文档] 所有源文件头部添加 MulanPSL v2 许可证声明
    - dist/index.html、src-tauri/src/*.rs、*.cjs 共 6 个源文件加入版权头

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,13 @@
+// Copyright (c) 2026 青山公仔
+// 轻壳 (Tauri Shell) is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
